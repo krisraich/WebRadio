74dba879352efc8006428f05970205d298faa104
Kris Raich <Krispin.Raich@fh-kufstein.ac.at>
2017-02-21 11:31:12 +0100

    changed readme.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # WebRadio
-Webradio for Raspberry Pi
+Webradio for Raspberry Pi with Webinterface
 
 ##Installation
-1. Install mplayer
-2. Create fifo file
-3. Run __java -jar Radio3.jar -c=pathToFifo__
+1. Install [mplayer](https://wiki.ubuntuusers.de/MPlayer/)
+2. Create a named pipe (aka fifo File) __mkfifo fifo__
+3. Run __java -jar Radio3.jar -c=pathToPipe__
--- src/webserver/CacheResponseHolder.java
+++ src/webserver/CacheResponseHolder.java
@@ -1,5 +1,6 @@
 package webserver;
 
+import java.util.Date;
 import radio3.Util;
 
 /**
@@ -10,20 +11,30 @@ public class CacheResponseHolder {
     
    private final byte[] responsedata;
    private final String mimetype;
-
-
+   private final long created;
+   private int called = 0;
 
     private CacheResponseHolder(String mimetype, byte[] responsedata) {
         this.responsedata = responsedata;
         this.mimetype = mimetype;
+        this.created = System.currentTimeMillis();
     }
 
     public byte[] getResponseData() {
+        called++;
         return responsedata;
     }
 
     public String getMimetype() {
         return mimetype;
+    }
+
+    public int getCalled() {
+        return called;
+    }
+
+    public Date getCreated() {
+        return new Date(this.created);
     }
    
     public static CacheResponseHolder createCacheResponseHolderFromURL(String requestURL, byte[] responsedata){
